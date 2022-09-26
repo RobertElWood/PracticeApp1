2aaf9416be4650cab90c09b1b8bcfc4aa570e928
Robert Woods <robert.woods@fulbrightmail.org>
2022-09-26 13:46:55 -0400

    Initial Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.AccessControl;
+
+public class Program
+{
+    public static void Main()
+    {
+        bool trigger = false;
+
+        while (!trigger)
+        {
+            Console.WriteLine("Please input the Radius of a circle. \n");
+
+
+            string input = Console.ReadLine();
+            double radius = double.Parse(input);
+
+            Console.WriteLine("\nRadius: " + radius);
+            Console.WriteLine();
+
+            double area = Math.PI * (radius * radius);
+
+            Console.WriteLine("\nArea: " + area);
+
+            Console.WriteLine("\nWould you like to measure another circle? Input 'yes' or 'y' to continue, or any other key to exit.\n");
+
+            string reply = Console.ReadLine();
+
+            if (reply == "y" || reply == "yes" || reply == "Yes")
+            {
+                trigger = false;
+                Console.WriteLine("\n");
+            }
+            else
+            {
+                Console.WriteLine("\nGoodbye!");
+                trigger = true;
+            }
+        }
+            
+    }
+}
